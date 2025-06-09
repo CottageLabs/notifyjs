0654caab141ad1472d25206869d0c7c4f5f9c783
kush.varade.19 <kush.varade.19@gmail.com>
2025-06-09 13:58:06 +0530

    All patterns test are working

--- patterns/accept.js
+++ patterns/accept.js
@@ -1,50 +1,8 @@
-/**
- * Pattern to represent an Accept notification
- * https://coar-notify.net/specification/1.0.0/accept/
- */
-
 import { NotifyPattern } from "../core/notify.js";
-
-function NestedPatternObjectMixin(Base) {
-  return class extends Base {
-    async getObject() {
-      const o = this.get_property("object");
-      if (o !== null && o !== undefined) {
-        const { COARNotifyFactory } = await import("../factory.js");
-        const nested = COARNotifyFactory.get_by_object(
-          JSON.parse(JSON.stringify(o)),
-          {
-            validate_stream_on_construct: false,
-            validate_properties: this.validate_properties,
-            validators: this.validators,
-            validation_context: null,
-          }
-        );
-        if (nested !== null) {
-          return nested;
-        }
-        const { NotifyObject } = await import("../core/notify.js");
-        return new NotifyObject({
-          stream: JSON.parse(JSON.stringify(o)),
-          validate_stream_on_construct: false,
-          validate_properties: this.validate_properties,
-          validators: this.validators,
-          validation_context: "object",
-        });
-      }
-      return null;
-    }
-
-    set object(value) {
-      this.set_property("object", value.doc);
-    }
-  };
-}
-
 import { ActivityStreamsTypes, Properties } from "../core/activitystreams2.js";
 import { ValidationError } from "../exceptions.js";
 
-export class Accept extends NestedPatternObjectMixin(NotifyPattern) {
+export class Accept extends NotifyPattern {
   static TYPE = ActivityStreamsTypes.ACCEPT;
 
   constructor(options = {}) {
@@ -54,27 +12,23 @@ export class Accept extends NestedPatternObjectMixin(NotifyPattern) {
 
   async validate() {
     const ve = new ValidationError();
-    try {
-      await super.validate();
-    } catch (superve) {
-      Object.assign(ve, superve);
+
+    // Validate required fields directly
+    if (!this.in_reply_to) {
+      ve.addError(Properties.IN_REPLY_TO, "inReplyTo is required");
     }
 
-    console.log("Accept.validate: in_reply_to =", this.in_reply_to);
-    const nestedObject = await this.getObject();
-    console.log("Accept.validate: nestedObject =", nestedObject);
-    console.log(
-      "Accept.validate: nestedObject.id =",
-      nestedObject ? nestedObject.id : null
-    );
+    const object = this.get_property("object");
+    if (!object) {
+      ve.addError(Properties.OBJECT, "object is required");
+    } else if (!object.id) {
+      ve.addError(Properties.OBJECT, "object.id is required");
+    }
 
-    this.required_and_validate(ve, Properties.IN_REPLY_TO, this.in_reply_to);
-
-    const objid = nestedObject ? nestedObject.id : null;
-    if (this.in_reply_to !== objid) {
+    if (this.in_reply_to && object?.id && this.in_reply_to !== object.id) {
       ve.addError(
         Properties.IN_REPLY_TO,
-        `Expected inReplyTo id to be the same as the nested object id. inReplyTo: ${this.in_reply_to}, object.id: ${objid}`
+        `inReplyTo must match object.id (${this.in_reply_to} != ${object.id})`
       );
     }
 
